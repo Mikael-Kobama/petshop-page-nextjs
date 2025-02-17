0621d8e305ede18a33c8bcc5c4f8580c979b5622
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-02-17 20:53:36 -0300

    Services updated

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -63,6 +63,13 @@ export function Services() {
     },
   });
 
+  function scrollPrev() {
+    emblaApi?.scrollPrev();
+  }
+  function scrollNext() {
+    emblaApi?.scrollNext();
+  }
+
   return (
     <section className="bg-white py-16">
       <div className="container -mx-auto px-4">
@@ -92,17 +99,38 @@ export function Services() {
                         </div>
                       </div>
 
-                      <div className="border-t">
-                        <div className="items-center gap-2 text-sm">
+                      <div className="border-t border-gray-700 pt-4 flex items-center justify-between">
+                        <div className="flex items-center gap-2 text-sm">
                           <Clock className="w-4 h-4" />
                           <span>{item.duration}</span>
                         </div>
+
+                        <a
+                          href="#"
+                          className="flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300"
+                        >
+                          <WhatsappLogo className="w-5 h-5" />
+                          Entrar em contato
+                        </a>
                       </div>
                     </article>
                   </div>
                 ))}
               </div>
             </div>
+
+            <button
+              className=" bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+              onClick={scrollPrev}
+            >
+              <ChevronLeft className="w-6 h-6 text-gray-600 " />
+            </button>
+            <button
+              className=" bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+              onClick={scrollNext}
+            >
+              <ChevronRight className="w-6 h-6 text-gray-600 " />
+            </button>
           </div>
         </div>
       </div>
